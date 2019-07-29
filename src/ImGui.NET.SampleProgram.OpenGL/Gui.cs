43b2fc4e64e97a86c77cabe72926919cb5885157
giawa <giawa@hotmail.com>
2019-07-28 21:31:42 -0700

    Create a sample OpenGL project.

---
+++ src/ImGui.NET.SampleProgram.OpenGL/Gui.cs
@@ -0,0 +1,213 @@
+﻿using OpenGL;
+using System;
+using System.IO;
+using System.Numerics;
+using System.Reflection;
+using System.Text;
+
+namespace ImGuiNET
+{
+    public static class Gui
+    {
+        private static ShaderProgram _shaderProgram;
+
+        private static int _texAttribLocation = 0, _projMatrixAttribLocation = 0;
+        private static int _vertPosAttribLocation = 0, _vertUVAttribLocation = 0, _vertColorAttribLocation = 0;
+
+        private static Texture _fontTexture;
+
+        private static uint _vboBuffer, _elementBuffer;
+
+        private static int _width, _height;
+
+        public static void Reshape(int width, int height)
+        {
+            _width = width;
+            _height = height;
+        }
+
+        private static string LoadEmbeddedShaderCode(string name)
+        {
+            string resourceName = name + ".glsl";
+            byte[] data = GetEmbeddedResourceBytes(resourceName);
+            return Encoding.UTF8.GetString(data);
+        }
+
+        private static byte[] GetEmbeddedResourceBytes(string resourceName)
+        {
+            Assembly assembly = typeof(Gui).Assembly;
+            var temp = assembly.GetManifestResourceNames();
+            using (Stream s = assembly.GetManifestResourceStream(resourceName))
+            {
+                byte[] ret = new byte[s.Length];
+                s.Read(ret, 0, (int)s.Length);
+                return ret;
+            }
+        }
+
+        public static void Init()
+        {
+            // compile the shader program
+            string vertexShader = LoadEmbeddedShaderCode("imgui-vertex");
+            string fragmentShader = LoadEmbeddedShaderCode("imgui-frag");
+            _shaderProgram = new ShaderProgram(vertexShader, fragmentShader);
+
+            _shaderProgram.Use();
+            _texAttribLocation = _shaderProgram["FontTexture"].Location;
+            _projMatrixAttribLocation = _shaderProgram["projection_matrix"].Location;
+            _vertPosAttribLocation = Gl.GetAttribLocation(_shaderProgram.ProgramID, "in_position");
+            _vertUVAttribLocation = Gl.GetAttribLocation(_shaderProgram.ProgramID, "in_texCoord");
+            _vertColorAttribLocation = Gl.GetAttribLocation(_shaderProgram.ProgramID, "in_color");
+
+            _vboBuffer = Gl.GenBuffer();
+            _elementBuffer = Gl.GenBuffer();
+
+            var imguiContext = ImGui.CreateContext();
+            ImGui.SetCurrentContext(imguiContext);
+            ImGui.GetIO().Fonts.AddFontDefault();
+            RecreateFontDeviceTexture();
+            ImGui.StyleColorsDark();
+
+            SetKeyMappings();
+        }
+
+        private static void SetKeyMappings()
+        {
+            ImGuiIOPtr io = ImGui.GetIO();
+            io.KeyMap[(int)ImGuiKey.Tab] = (int)Veldrid.Sdl2.SDL_Keycode.SDLK_TAB;
+            io.KeyMap[(int)ImGuiKey.LeftArrow] = 47;
+            io.KeyMap[(int)ImGuiKey.RightArrow] = 48;
+            io.KeyMap[(int)ImGuiKey.UpArrow] = 45;
+            io.KeyMap[(int)ImGuiKey.DownArrow] = 46;
+            io.KeyMap[(int)ImGuiKey.PageUp] = 56;
+            io.KeyMap[(int)ImGuiKey.PageDown] = 57;
+            io.KeyMap[(int)ImGuiKey.Home] = 58;
+            io.KeyMap[(int)ImGuiKey.End] = 59;
+            io.KeyMap[(int)ImGuiKey.Delete] = (int)Veldrid.Sdl2.SDL_Keycode.SDLK_DELETE;
+            io.KeyMap[(int)ImGuiKey.Backspace] = (int)Veldrid.Sdl2.SDL_Keycode.SDLK_BACKSPACE;
+            io.KeyMap[(int)ImGuiKey.Enter] = 49;
+            io.KeyMap[(int)ImGuiKey.Escape] = (int)Veldrid.Sdl2.SDL_Keycode.SDLK_ESCAPE;
+            io.KeyMap[(int)ImGuiKey.A] = (int)Veldrid.Sdl2.SDL_Keycode.SDLK_a;
+            io.KeyMap[(int)ImGuiKey.C] = (int)Veldrid.Sdl2.SDL_Keycode.SDLK_c;
+            io.KeyMap[(int)ImGuiKey.V] = (int)Veldrid.Sdl2.SDL_Keycode.SDLK_v;
+            io.KeyMap[(int)ImGuiKey.X] = (int)Veldrid.Sdl2.SDL_Keycode.SDLK_x;
+            io.KeyMap[(int)ImGuiKey.Y] = (int)Veldrid.Sdl2.SDL_Keycode.SDLK_y;
+            io.KeyMap[(int)ImGuiKey.Z] = (int)Veldrid.Sdl2.SDL_Keycode.SDLK_z;
+        }
+
+        public static void Dispose()
+        {
+            // dispose of all of the resources that were created
+            _fontTexture.Dispose();
+            _shaderProgram.DisposeChildren = true;
+            _shaderProgram.Dispose();
+        }
+
+        public static void RecreateFontDeviceTexture()
+        {
+            ImGuiIOPtr io = ImGui.GetIO();
+
+            // get the font texture from imgui
+            IntPtr pixels;
+            int width, height, bytesPerPixel;
+            io.Fonts.GetTexDataAsRGBA32(out pixels, out width, out height, out bytesPerPixel);
+            io.Fonts.SetTexID((IntPtr)1);
+
+            // take the bytes returned by the font texture and then turn it into a texture
+            _fontTexture = new Texture(pixels, width, height, PixelFormat.Rgba, PixelInternalFormat.Rgba);
+
+            io.Fonts.ClearTexData();
+        }
+
+        public static void RenderImDrawData(ImDrawDataPtr draw_data)
+        {
+            if (draw_data.CmdListsCount == 0)
+            {
+                return;
+            }
+
+            // really these properties should be read and then reset back to what they were
+            Gl.Enable(EnableCap.Blend);
+            Gl.BlendEquation(BlendEquationMode.FuncAdd);
+            Gl.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+            Gl.Disable(EnableCap.CullFace);
+            Gl.Disable(EnableCap.DepthTest);
+            Gl.Enable(EnableCap.ScissorTest);
+
+            ImGuiIOPtr io = ImGui.GetIO();
+            io.DisplaySize = new Vector2(_width, _height);
+
+            Matrix4 mvp = Matrix4.CreateOrthographicOffCenter(0f, io.DisplaySize.X, io.DisplaySize.Y, 0.0f, -1.0f, 1.0f);
+
+            _shaderProgram.Use();
+            Gl.Uniform1f(_texAttribLocation, 0);
+            Gl.UniformMatrix4fv(_projMatrixAttribLocation, mvp);
+
+            Gl.BindBuffer(BufferTarget.ArrayBuffer, _vboBuffer);
+            Gl.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBuffer);
+
+            Gl.EnableVertexAttribArray(_vertPosAttribLocation);
+            Gl.EnableVertexAttribArray(_vertUVAttribLocation);
+            Gl.EnableVertexAttribArray(_vertColorAttribLocation);
+            Gl.VertexAttribPointer(_vertPosAttribLocation, 2, VertexAttribPointerType.Float, false, 20, (IntPtr)0);
+            Gl.VertexAttribPointer(_vertUVAttribLocation, 2, VertexAttribPointerType.Float, false, 20, (IntPtr)8);
+            Gl.VertexAttribPointer(_vertColorAttribLocation, 4, VertexAttribPointerType.UnsignedByte, true, 20, (IntPtr)16);
+
+            var clip_off = draw_data.DisplayPos;         // (0,0) unless using multi-viewports
+            var clip_scale = draw_data.FramebufferScale; // (1,1) unless using retina display which are often (2,2)
+
+            for (int n = 0; n < draw_data.CmdListsCount; n++)
+            {
+                ImDrawListPtr cmd_list = draw_data.CmdListsRange[n];
+
+                Gl.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(cmd_list.VtxBuffer.Size * 20), cmd_list.VtxBuffer.Data, BufferUsageHint.DynamicDraw);
+                Gl.BufferData(BufferTarget.ElementArrayBuffer, (IntPtr)(cmd_list.IdxBuffer.Size * 2), cmd_list.IdxBuffer.Data, BufferUsageHint.DynamicDraw);
+
+                int idx_offset = 0;
+                int vtx_offset = 0;
+
+                for (int cmd_i = 0; cmd_i < cmd_list.CmdBuffer.Size; cmd_i++)
+                {
+                    var pcmd = cmd_list.CmdBuffer[cmd_i];
+
+                    if (pcmd.UserCallback != IntPtr.Zero)
+                    {
+                        Console.WriteLine("Unknown");
+                    }
+                    else
+                    {
+                        Vector4 clip_rect;
+                        clip_rect.X = (pcmd.ClipRect.X - clip_off.X) * clip_scale.X;
+                        clip_rect.Y = (pcmd.ClipRect.Y - clip_off.Y) * clip_scale.Y;
+                        clip_rect.Z = (pcmd.ClipRect.Z - clip_off.X) * clip_scale.X;
+                        clip_rect.W = (pcmd.ClipRect.W - clip_off.Y) * clip_scale.Y;
+
+                        if (clip_rect.X < _width && clip_rect.Y < _height && clip_rect.Z >= 0.0f && clip_rect.W >= 0.0f)
+                        {
+                            // apply scissor/clipping rectangle
+                            Gl.Scissor((int)clip_rect.X, (int)(_height - clip_rect.W), (int)(clip_rect.Z - clip_rect.X), (int)(clip_rect.W - clip_rect.Y));
+
+                            // set the requested texture
+                            Gl.BindTexture(TextureTarget.Texture2D, (uint)pcmd.TextureId);
+
+                            Gl.DrawElementsBaseVertex(BeginMode.Triangles, (int)pcmd.ElemCount, DrawElementsType.UnsignedShort, (IntPtr)(idx_offset * 2), vtx_offset);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Missed test");
+                        }
+
+                        idx_offset += (int)pcmd.ElemCount;
+                    }
+                }
+
+                vtx_offset += cmd_list.VtxBuffer.Size;
+            }
+
+            Gl.Disable(EnableCap.ScissorTest);
+            Gl.Enable(EnableCap.DepthTest);
+            Gl.Enable(EnableCap.CullFace);
+            Gl.Disable(EnableCap.Blend);
+        }
+    }
+}
---
+++ src/ImGui.NET.SampleProgram.OpenGL/Program.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using OpenGL;
+using Veldrid.Sdl2;
+
+namespace ImGuiNET
+{
+    class Program
+    {
+        private static int _width = 1280, _height = 720;
+        private static bool mouseleft, mouseright;
+        private static int mouseX, mouseY, mouseWheel;
+
+        static unsafe void Main(string[] args)
+        {
+            if (Sdl2Native.SDL_Init((SDLInitFlags)62001) < 0)
+            {
+                Console.WriteLine("SDL failed to init.");
+                return;
+            }
+
+            Sdl2Native.SDL_GL_SetAttribute(SDL_GLAttribute.DoubleBuffer, 1);
+            Sdl2Native.SDL_GL_SetAttribute(SDL_GLAttribute.DepthSize, 24);
+            Sdl2Native.SDL_GL_SetAttribute(SDL_GLAttribute.ContextMajorVersion, 3);
+
+            IntPtr window = Sdl2Native.SDL_CreateWindow("Imgui.NET OpenGL Sample Program", 128, 128, _width, _height, SDL_WindowFlags.OpenGL | SDL_WindowFlags.Shown | SDL_WindowFlags.Resizable);
+
+            if (window == IntPtr.Zero)
+            {
+                Console.WriteLine("SDL could not create a window.");
+                return;
+            }
+
+            IntPtr context = Sdl2Native.SDL_GL_CreateContext(window);
+
+            if (context == IntPtr.Zero)
+            {
+                Console.WriteLine("SDL could not create a valid OpenGL context.");
+                return;
+            }
+
+            Sdl2Native.SDL_GL_SetSwapInterval(1);
+
+            Gui.Init();
+            Gui.Reshape(_width, _height);
+
+            SDL_Event sdlEvent;
+            bool running = true;
+
+            while (running)
+            {
+                while (Sdl2Native.SDL_PollEvent(&sdlEvent) != 0)
+                {
+                    if (sdlEvent.type == SDL_EventType.Quit)
+                    {
+                        running = false;
+                    }
+                    else if (sdlEvent.type == SDL_EventType.TextInput)
+                    {
+                        SDL_TextInputEvent textEvent = Unsafe.Read<SDL_TextInputEvent>(&sdlEvent);
+
+                        _keyCharPresses.Clear();
+                        var chars = TextInputToChars(textEvent);
+                        foreach (var c in chars)
+                        {
+                            if (!_keyCharPresses.Contains(c)) _keyCharPresses.Add(c);
+                        }
+                    }
+                    else if (sdlEvent.type == SDL_EventType.KeyDown)
+                    {
+                        SDL_KeyboardEvent keyEvent = Unsafe.Read<SDL_KeyboardEvent>(&sdlEvent);
+
+                        switch (keyEvent.keysym.sym)
+                        {
+                            case SDL_Keycode.SDLK_LCTRL:
+                                _lcontrolDown = true;
+                                break;
+                            case SDL_Keycode.SDLK_RCTRL:
+                                _rcontrolDown = true;
+                                break;
+                            case SDL_Keycode.SDLK_LSHIFT:
+                                _lshiftDown = true;
+                                break;
+                            case SDL_Keycode.SDLK_RSHIFT:
+                                _rshiftDown = true;
+                                break;
+                            case SDL_Keycode.SDLK_LALT:
+                                _laltDown = true;
+                                break;
+                            case SDL_Keycode.SDLK_RALT:
+                                _raltDown = true;
+                                break;
+                            case SDL_Keycode.SDLK_APPLICATION:
+                                _winKeyDown = true;
+                                break;
+                            default:
+                                var c = KeycodeToChar(keyEvent.keysym.sym);
+                                if (c != ' ') _keysDown.Add(c);
+                                break;
+                        }
+                    }
+                    else if (sdlEvent.type == SDL_EventType.KeyUp)
+                    {
+                        SDL_KeyboardEvent keyEvent = Unsafe.Read<SDL_KeyboardEvent>(&sdlEvent);
+
+                        switch (keyEvent.keysym.sym)
+                        {
+                            case SDL_Keycode.SDLK_LCTRL:
+                                _lcontrolDown = false;
+                                break;
+                            case SDL_Keycode.SDLK_RCTRL:
+                                _rcontrolDown = false;
+                                break;
+                            case SDL_Keycode.SDLK_LSHIFT:
+                                _lshiftDown = false;
+                                break;
+                            case SDL_Keycode.SDLK_RSHIFT:
+                                _rshiftDown = false;
+                                break;
+                            case SDL_Keycode.SDLK_LALT:
+                                _laltDown = false;
+                                break;
+                            case SDL_Keycode.SDLK_RALT:
+                                _raltDown = false;
+                                break;
+                            case SDL_Keycode.SDLK_APPLICATION:
+                                _winKeyDown = false;
+                                break;
+                            default:
+                                var c = KeycodeToChar(keyEvent.keysym.sym);
+                                if (c != ' ') _keysUp.Add(c);
+                                break;
+                        }
+                    }
+                    else if (sdlEvent.type == SDL_EventType.MouseButtonDown)
+                    {
+                        SDL_MouseButtonEvent buttonEvent = Unsafe.Read<SDL_MouseButtonEvent>(&sdlEvent);
+
+                        if (buttonEvent.button == SDL_MouseButton.Left) mouseleft = true;
+                        if (buttonEvent.button == SDL_MouseButton.Right) mouseright = true;
+                    }
+                    else if (sdlEvent.type == SDL_EventType.MouseButtonUp)
+                    {
+                        SDL_MouseButtonEvent buttonEvent = Unsafe.Read<SDL_MouseButtonEvent>(&sdlEvent);
+
+                        if (buttonEvent.button == SDL_MouseButton.Left) mouseleft = false;
+                        if (buttonEvent.button == SDL_MouseButton.Right) mouseright = false;
+                    }
+                    else if (sdlEvent.type == SDL_EventType.MouseMotion)
+                    {
+                        SDL_MouseMotionEvent motionEvent = Unsafe.Read<SDL_MouseMotionEvent>(&sdlEvent);
+
+                        mouseX = motionEvent.x;
+                        mouseY = motionEvent.y;
+                    }
+                    else if (sdlEvent.type == SDL_EventType.MouseWheel)
+                    {
+                        SDL_MouseWheelEvent wheelEvent = Unsafe.Read<SDL_MouseWheelEvent>(&sdlEvent);
+
+                        mouseWheel = wheelEvent.y;
+                    }
+                    else if (sdlEvent.type == SDL_EventType.WindowEvent)
+                    {
+                        SDL_WindowEvent windowEvent = Unsafe.Read<SDL_WindowEvent>(&sdlEvent);
+
+                        switch (windowEvent.@event)
+                        {
+                            case SDL_WindowEventID.Resized:
+                                OnReshape(windowEvent.data1, windowEvent.data2);
+                                break;
+                        }
+                    }
+                }
+
+                OnRenderFrame();
+                Sdl2Native.SDL_GL_SwapWindow(window);
+            }
+
+            Gui.Dispose();
+            Sdl2Native.SDL_GL_DeleteContext(context);
+            Sdl2Native.SDL_DestroyWindow(window);
+        }
+
+        private static char KeycodeToChar(SDL_Keycode keycode)
+        {
+            switch (keycode)
+            {
+                case SDL_Keycode.SDLK_TAB:
+                case SDL_Keycode.SDLK_DELETE:
+                case SDL_Keycode.SDLK_BACKSPACE:
+                case SDL_Keycode.SDLK_ESCAPE:
+                    return (char)keycode;
+                case SDL_Keycode.SDLK_UP:
+                    return (char)45;
+                case SDL_Keycode.SDLK_DOWN:
+                    return (char)46;
+                case SDL_Keycode.SDLK_LEFT:
+                    return (char)47;
+                case SDL_Keycode.SDLK_RIGHT:
+                    return (char)48;
+                case SDL_Keycode.SDLK_PAGEUP:
+                    return (char)56;
+                case SDL_Keycode.SDLK_PAGEDOWN:
+                    return (char)57;
+                case SDL_Keycode.SDLK_HOME:
+                    return (char)58;
+                case SDL_Keycode.SDLK_END:
+                    return (char)59;
+                case SDL_Keycode.SDLK_RETURN:
+                    return (char)49;
+            }
+
+            return ' ';
+        }
+
+        private static readonly char[] _emptyCharArray = new char[0];
+
+        private unsafe static char[] TextInputToChars(SDL_TextInputEvent textInputEvent)
+        {
+            int byteCount = 0;
+            // Loop until the null terminator is found or the max size is reached.
+            while (byteCount < SDL_TextInputEvent.MaxTextSize && textInputEvent.text[byteCount++] != 0)
+            { }
+
+            if (byteCount > 1)
+            {
+                // We don't want the null terminator.
+                byteCount -= 1;
+                byte[] bytesArray = new byte[byteCount];
+                Marshal.Copy((IntPtr)textInputEvent.text, bytesArray, 0, byteCount);
+                int charCount = Encoding.UTF8.GetCharCount(textInputEvent.text, (int)byteCount);
+                char[] charsPtr = new char[charCount];
+                Encoding.UTF8.GetChars(bytesArray, 0, byteCount, charsPtr, 0);
+                return charsPtr;
+            }
+            else return _emptyCharArray;
+        }
+
+        private static void OnRenderFrame()
+        {
+            Gl.Viewport(0, 0, _width, _height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            UpdateImGuiInput();
+
+            ImGui.NewFrame();
+            ImGui.ShowDemoWindow();
+
+            ImGui.Render();
+
+            var drawData = ImGui.GetDrawData();
+            Gui.RenderImDrawData(drawData);
+        }
+
+        private static void OnReshape(int width, int height)
+        {
+            _width = width;
+            _height = height;
+            Gui.Reshape(_width, _height);
+        }
+
+        private static bool _lcontrolDown, _lshiftDown, _laltDown, _rcontrolDown, _rshiftDown, _raltDown, _winKeyDown;
+        private static HashSet<char> _keyCharPresses = new HashSet<char>();
+        private static HashSet<char> _keysDown = new HashSet<char>();
+        private static HashSet<char> _keysUp = new HashSet<char>();
+
+        private static void UpdateImGuiInput()
+        {
+            ImGuiIOPtr io = ImGui.GetIO();
+
+            Vector2 mousePosition = new Vector2(mouseX, mouseY);
+
+            io.MouseDown[0] = mouseleft;
+            io.MouseDown[1] = mouseright;
+            io.MouseDown[2] = false;
+            io.MousePos = mousePosition;
+            io.MouseWheel = mouseWheel;
+
+            mouseWheel = 0;
+
+            foreach (var key in _keyCharPresses)
+                io.AddInputCharacter(key);
+            _keyCharPresses.Clear();
+
+            foreach (var key in _keysDown)
+                io.KeysDown[key] = true;
+            foreach (var key in _keysUp)
+                io.KeysDown[key] = false;
+
+            _keysDown.Clear();
+            _keysUp.Clear();
+
+            io.KeyCtrl = _lcontrolDown | _rcontrolDown;
+            io.KeyAlt = _laltDown | _raltDown;
+            io.KeyShift = _lshiftDown | _rshiftDown;
+            io.KeySuper = _winKeyDown;
+        }
+    }
+}
